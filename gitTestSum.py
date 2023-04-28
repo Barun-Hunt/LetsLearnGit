6507038e16963b6e437a4b00cdf785c50b4ca54f
Barun Chakrabarty <barun.chakrabarty06@gmail.com>
2023-04-28 16:40:25 +0530

    Commiting the modified gitTestSum file

--- gitTestSum.py
+++ gitTestSum.py
@@ -1,3 +1,4 @@
 a=int(input("Enter the first number\n"))
 b=int(input("Enter the second number\n"))
-print("The sum of these numbers is ",a+b)
+c=int(input("Enter the third number\n"))
+print("The sum of these numbers is ",a+b+c)
